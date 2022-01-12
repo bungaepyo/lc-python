ac56bbdf807536f271a7493c07357da2a73005c8
bungaepyo <bungaepyo@gmail.com>
2022-01-12 16:00:29 +0900

    7. Reverse Integer

--- reverse-integer.py
+++ reverse-integer.py
@@ -26,3 +26,37 @@ Constraints:
 
 -231 <= x <= 231 - 1
 '''
+
+'''
+------------------------------------------------------------------------
+Solution 1: visit and store digits & check overflow at the end
+Time: O(log(n)) - visit each digit
+Space: O(log(n)) - store each digit
+
+Runtime: 15 ms
+Memory: 13.4 MB
+
+This is my original solution. I visit each digit using a carry that is later /= 10.
+I pick the last number of the carry by doing %= and add the number to a list.
+Using list length & index of each number stored in the list, I decide the digit and append to the result integer.
+Applying sign and checking for overflow is done right before returning the result.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def reverse(self, x):
+        digits = []
+        carry = x if x >= 0 else -1*x
+        sign = 1 if x >= 0 else -1
+        
+        while carry > 0:
+            rem = carry % 10
+            digits.append(rem)
+            carry /= 10
+            
+        res = 0
+        length = len(digits)
+        for i in range(length):
+            res += 10**(length-1-i)*digits[i]
+        
+        res *= sign
+        return res if (res >= -1*2**31 and res < 2**31-1) else 0
