71770cefede2b23b78c6cfb7f63266d9a95ca010
bungaepyo <bungaepyo@gmail.com>
2022-01-12 18:54:46 +0900

    8. String to Integer (atoi)

--- string-to-integer-atoi.py
+++ string-to-integer-atoi.py
@@ -69,3 +69,47 @@ Constraints:
 0 <= s.length <= 200
 s consists of English letters (lower-case and upper-case), digits (0-9), ' ', '+', '-', and '.'.
 '''
+
+'''
+------------------------------------------------------------------------
+Solution 1: Follow the rules
+Time: O(n)
+Space: O(1)
+
+Runtime: 24 ms
+Memory: 13.4 MB
+
+This is a solution precisely following all the rules, considering edge cases.
+In my opinion this question is not good.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def myAtoi(self, s):
+        sign = 1 
+        result = 0
+        index = 0
+        n = len(s)
+        
+        INT_MAX = pow(2,31) - 1 
+        INT_MIN = -pow(2,31)
+        
+        while index < n and s[index] == ' ':
+            index += 1
+        
+        if index < n and s[index] == '+':
+            sign = 1
+            index += 1
+        elif index < n and s[index] == '-':
+            sign = -1
+            index += 1
+        
+        while index < n and s[index].isdigit():
+            digit = int(s[index])
+            
+            if ((result > INT_MAX // 10) or (result == INT_MAX // 10 and digit > INT_MAX % 10)):
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            result = 10 * result + digit
+            index += 1
+        
+        return sign * result
