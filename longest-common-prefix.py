c432740eea0a3e5698c444d2be3a8cdf084ea829
bungaepyo <bungaepyo@gmail.com>
2022-01-18 23:24:44 +0900

    14. Longest Common Prefix

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -26,14 +26,14 @@ strs[i] consists of only lower-case English letters.
 
 '''
 ------------------------------------------------------------------------
-Solution 1 - enumerate()
+Solution 1 - Vertical Scanning, enumerate()
 Time: O(n)
 Space: O(1)
 
 Runtime: 16 ms
 Memory: 13.6 MB
 
-Best solution I've seen so far, but very pythonic.
+Best solution I've seen so far using vertical scanning method, but very pythonic.
 First, get the shortest length string from the list as longest common prefix cannot be longer than that.
 Second, use enumerate() and iterate through each index and char.
 Third, within the index & char for loop, iterate through the list to see every string's ith index matches char.
@@ -53,7 +53,33 @@ class Solution(object):
 
 '''
 ------------------------------------------------------------------------
-Solution 2 - zip()
+Solution 2 - Vertical Scanning
+Time: O(n)
+Space: O(1)
+
+Runtime: 28 ms
+Memory: 14 MB
+
+Another vertical scanning solution, but less pythonic.
+Sets default to the first string in the list, and iterates through its indices.
+In a nested for loop, compares the characters of the same index.
+Checks (1) if any string is shorter than the first one OR (2) characters don't match => return strs[0][:i]
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or strs[j][i] != c:
+                    return strs[0][:i]
+        return strs[0]
+
+'''
+------------------------------------------------------------------------
+Solution 3 - zip()
 Time: O(n)
 Space: O(1)
 
