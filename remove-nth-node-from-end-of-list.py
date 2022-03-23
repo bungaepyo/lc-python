64c3c5d5d21b1a854467ad88fe66c274015754c9
bungaepyo <bungaepyo@gmail.com>
2022-03-22 23:55:54 -0400

    19. Remove Nth Node From End of List

--- remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list.py
@@ -27,3 +27,42 @@ The number of nodes in the list is sz.
 0 <= Node.val <= 100
 1 <= n <= sz
 '''
+
+'''
+------------------------------------------------------------------------
+Solution 1: two pointer method
+Time: O(n)
+Space: O(1)
+
+Runtime: 43 ms
+Memory: 13.6 MB
+
+This is a standard solution using the two pointer method: fast & slow.
+Since the node we want to delete is Nth from the end, and the two pointers are initialized on head,
+advancing fast Nth times prior to advancing slow will leave (size-n-1) space.
+After this, all we have to do is advance slow while fast is not pointing null,
+and skip the node after slow when the loop ends.
+Lastly, since fast & slow are initialized on the first node, we need to account for an edge case
+where we actually want to delete the first node of the linked list.
+After fast first advances, if fast is pointing to null, it means that n = size.
+This means that we want to delete the first node, which is (size)th from the end.
+We should simply return head.next in this case.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        
+        for _ in range(n):
+            fast = fast.next
+        
+        if not fast:
+            return head.next
+        
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+        
+        return head
