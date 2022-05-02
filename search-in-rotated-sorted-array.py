d88a3d579f3f56b49c8c556d73f94bc9e55203ec
bungaepyo <bungaepyo@gmail.com>
2022-05-02 00:04:49 -0400

    33. Search in Rotated Sorted Array

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -109,3 +109,47 @@ class Solution(object):
         #if target is bigger than first element, do binary search from start to pivot
         return binarySearch(0, pivot)
 
+'''
+------------------------------------------------------------------------
+Solution 2 - One-Pass Binary Search
+Time: O(log n)
+Space: O(1)
+
+Runtime: 46 ms
+Memory: 13.9  MB
+
+This is a solution using binary search, but simplified because it's only one-pass.
+The key to this solution is understanding:
+  (1) two diff scenarios => nums[mid] is larger than nums[start] AND nums[mid] is smaller than nums[start]
+  (2) we need to keep looking for a sorted part where it contains target
+In a while loop where end index is always greater than or equal to start index,
+every iteration we get a new mid and see if nums[mid] is equal to target, just like normal binary search.
+However, we start looking if current mid is in the un-rotated part or the rotated part.
+If current mid is in the rotated part (nums[start] <= nums[mid]):
+    this means we can see if target is between start and mid, which is sorted
+If current mid is in the un-rotated part (nums[start] > nums[mid]):
+    this means we can see if target is between mid and end, which is also sorted
+
+In either cases, if we successfully find the sorted part that contains target,
+it decreases the time for rest of the process becuase now it's straigforward doing normal binary search.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def search(self, nums, target):
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = start + (end-start)/2
+            if nums[mid] == target:
+                return mid
+            elif nums[start] <= nums[mid]:
+                if target >= nums[start] and target <= nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target >= nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
