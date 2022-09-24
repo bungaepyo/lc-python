14d8a5c7a7d338904fd2578f4b27bf074a606e82
bungaepyo <bungaepyo@gmail.com>
2022-09-23 20:53:55 -0400

    19. Remove Nth Node From End of List

--- remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list.py
@@ -66,3 +66,80 @@ class Solution(object):
         slow.next = slow.next.next
         
         return head
+
+'''
+------------------------------------------------------------------------
+Solution 2: two pointer method, dummy head
+Time: O(n)
+Space: O(1)
+
+Runtime: 38 ms
+Memory: 13.3 MB
+
+This is a two pointer technique solution that basically has the same idea
+as above solution, but instead uses a dummy head. The reason we want a dummy
+node is because we do not want to specifically accomodate for the edge
+case of removing the first node of the linked list. If we do everything the same
+way but return dummy.next, we shall be good because if we wanted to remove
+the first node, slow.next = slow.next.next will do it because slow was initially
+assigned as dummy.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        if head.next == None:
+            return None
+
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        slow = dummy
+        fast = dummy
+        
+        for _ in range(n):
+            fast = fast.next
+        
+        while fast.next != None:
+            slow = slow.next
+            fast = fast.next
+        
+        slow.next = slow.next.next
+        
+        return dummy.next
+
+'''
+------------------------------------------------------------------------
+Solution 2: two pass
+Time: O(n)
+Space: O(1)
+
+Runtime: 21 ms
+Memory: 13.4 MB
+
+This solution takes a different approach using a two pass method, although it
+does use the dummy head. First pass is for measuring the length of the linked list.
+Then we subtract n from length, and until remaining length runs out, we
+would advance first. We will be pointing at the previous node of the node that
+we want to delete since we subtracted n from length. Thus skip the next node
+and return dummy.next.
+------------------------------------------------------------------------
+'''
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        length = 0
+
+        first = head
+        while first != None:
+            length += 1
+            first = first.next
+        
+        length -= n
+        
+        while length > 0:
+            length -= 1
+            first = first.next
+            
+        first.next = first.next.next
+        return dummy.next
